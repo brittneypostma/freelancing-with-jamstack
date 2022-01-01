55a75a7f6931fa838fc92774b9ed9de9fad525c0
Brittney Postma <bdesignedwebdev@gmail.com>
2022-01-01 21:03:20 +0000

    deleted layouts

--- src/routes/__layout.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import '@styles/styles.css'
-</script>
-
-<main class="max-w-4xl mx-auto grid grid-cols-1 gap-10 lg:gap-20">
-	<slot />
-</main>
--- src/routes/tracks/__layout.reset.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-	import '@styles.css'
-</script>
-<!-- <main class="max-w-text mx-auto grid grid-cols-1 gap-10"> -->
-	<slot />
-<!-- </main> -->
