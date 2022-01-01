017f8d655cd2544a39f6856f30a13605e63ae82d
Brittney Postma <bdesignedwebdev@gmail.com>
2022-01-01 21:16:32 +0000

    seo fix

--- src/lib/layout/SEO.svelte
+++ src/lib/layout/SEO.svelte
@@ -1,8 +1,8 @@
-<!-- <script>
+<script>
     export let title
-    export let ogImg
-    export let desc
-</script> -->
+    // export let ogImg
+    // export let desc
+</script>
 
 <svelte:head>
     <title>{title}</title>
